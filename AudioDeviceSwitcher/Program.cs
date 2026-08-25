d43fef3272c4ca65857cba5e71afb1359d461163
Kwan-desu <briankwan555@gmail.com>
2026-08-25 10:19:21 +0800

    Hotfix: Make portable executable truly self-contained with native DLL extraction

--- AudioDeviceSwitcher/Program.cs
+++ AudioDeviceSwitcher/Program.cs
@@ -11,8 +11,6 @@ namespace AudioDeviceSwitcher
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
             // Cleanup portable update leftover (.old)
